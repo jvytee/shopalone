ac66002975ee0adafc9ce9a0b2798cceba9adca3
Julian Theis <julian.theis@posteo.de>
2020-03-21 21:24:28 +0100

    Fix warnings, shorten routing

--- src/shopalone.py
+++ src/shopalone.py
@@ -2,18 +2,21 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return 'shopalone'
+
 
 @app.route('/market/id/<int:key>', methods=['GET', 'POST'])
 def market_id(key: int):
     return str(key)
 
-@app.route('/market/postcode/<string:code>')
-def postcode(code: str):
-    return code
 
+@app.route('/market/postcode/<string:code>')
 @app.route('/market/postcode/<string:code>/<int:timestamp>')
-def postcode_timestamp(code: str, timestamp: int):
+def postcode_timestamp(code: str, timestamp: int = None):
+    if timestamp is None:
+        return code
+
     return f'{code} at {timestamp}'
