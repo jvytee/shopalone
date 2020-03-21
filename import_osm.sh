bfbb4c11e7681c59255fa8610676f7efb9041411
Julian Theis <julian.theis@posteo.de>
2020-03-21 15:35:03 +0100

    Quicksave postgres setup

---
+++ import_osm.sh
@@ -0,0 +1,5 @@
+osmosis --read-pbf koeln-regbez-latest.osm.pbf \
+    --tag-filter reject-relations \
+    --tag-filter accept-nodes shop=convenience,general,supermarket \
+    --tag-filter accept-ways shop=convenience,general,supermarket \
+    --write-pgsql database=shopalone host=127.0.0.1 user=postgres password=postgres
