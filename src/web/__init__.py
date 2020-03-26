b0b42f7c5c60bf5195a0b76b1ca4e1857dfe4fc1
Julian Theis <julian.theis@posteo.de>
2020-03-26 21:57:27 +0100

    Add milligram css framework

--- src/web/__init__.py
+++ src/web/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-blue = Blueprint("web", __name__, template_folder="templates")
+blue = Blueprint("web", __name__, static_folder="static", template_folder="templates")
 
 @blue.route("/")
 def index():
