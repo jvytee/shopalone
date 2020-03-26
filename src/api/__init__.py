227ad5969989c5f80fa65a93ea0184b4060ced74
Julian Theis <julian.theis@posteo.de>
2020-03-26 21:42:02 +0100

    Refactor app to use flask blueprints

---
+++ src/api/__init__.py
@@ -0,0 +1,51 @@
+from flask import Blueprint, abort, jsonify, request
+
+import controller
+
+blue = Blueprint("api", __name__)
+
+
+@blue.route("/")
+def index():
+    return '<a href="https://github.com/jvytee/shopalone">shopalone</a>'
+
+
+@blue.route("/market/<int:market_id>")
+def market(market_id: int):
+    result = controller.get_market(market_id)
+    if result is not None:
+        return result.to_dict()
+
+    abort(404)
+
+
+@blue.route("/visit/<int:market_id>")
+def visit(market_id: int):
+    results = [result.to_dict() for result in controller.get_visits(market_id)]
+    return jsonify(results)
+
+
+@blue.route("/visit/<int:market_id>/<int:timestamp>", methods=["GET", "POST"])
+def visit_time(market_id: int, timestamp: int):
+    if request.method == "GET":
+        results = [result.to_dict() for result in controller.get_visits_time(market_id, timestamp)]
+        return jsonify(results)
+
+    if request.method == "POST":
+        result = controller.add_visit(market_id, timestamp)
+        if result is not None:
+            return result.to_dict()
+
+        abort(404)
+
+
+@blue.route("/postcode/<string:code>")
+def postcode(code: str):
+    results = [result.to_dict() for result in controller.get_postcode(code)]
+
+    return jsonify(results)
+
+
+@blue.route("/postcode/<string:code>/<int:timestamp>")
+def postcode_time(code: str, timestamp: int):
+    abort(501)
--- src/shopalone.py
+++ src/shopalone.py
@@ -1,54 +1,12 @@
 from flask import Flask, abort, jsonify, request
 
 import database
-import controller
+import api
+import web
 
 app = Flask(__name__)
 app.teardown_appcontext(database.close_session)
 app.cli.add_command(database.init_db)
 
-
-@app.route("/")
-def index():
-    return '<a href="https://github.com/jvytee/shopalone">shopalone</a>'
-
-
-@app.route("/market/<int:market_id>")
-def market(market_id: int):
-    result = controller.get_market(market_id)
-    if result is not None:
-        return result.to_dict()
-
-    abort(404)
-
-
-@app.route("/visit/<int:market_id>")
-def visit(market_id: int):
-    results = [result.to_dict() for result in controller.get_visits(market_id)]
-    return jsonify(results)
-
-
-@app.route("/visit/<int:market_id>/<int:timestamp>", methods=["GET", "POST"])
-def visit_time(market_id: int, timestamp: int):
-    if request.method == "GET":
-        results = [result.to_dict() for result in controller.get_visits_time(market_id, timestamp)]
-        return jsonify(results)
-
-    if request.method == "POST":
-        result = controller.add_visit(market_id, timestamp)
-        if result is not None:
-            return result.to_dict()
-
-        abort(404)
-
-
-@app.route("/postcode/<string:code>")
-def postcode(code: str):
-    results = [result.to_dict() for result in controller.get_postcode(code)]
-
-    return jsonify(results)
-
-
-@app.route("/postcode/<string:code>/<int:timestamp>")
-def postcode_time(code: str, timestamp: int):
-    abort(501)
+app.register_blueprint(api.blue)
+app.register_blueprint(web.blue, url_prefix="/web")
