7b1b72c216a09b07e2554215b3a2d3a87cc4289d
Julian Theis <julian.theis@posteo.de>
2020-03-29 22:35:48 +0200

    Render list of supermarkets for postcode

--- src/web/__init__.py
+++ src/web/__init__.py
@@ -1,8 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+
+import controller
 
 blueprint = Blueprint("web", __name__, static_folder="static", template_folder="templates")
 
 
 @blueprint.route("/")
 def index():
-    return render_template("index.html")
+    markets = None
+    if "postcode" in request.args:
+        markets = controller.get_postcode(request.args.get("postcode"))
+
+    return render_template("index.html", markets=markets)
