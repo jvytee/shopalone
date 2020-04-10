73bb5d24e49f1d253c4f193d818f86d5b9a7d601
Julian Theis <julian.theis@posteo.de>
2020-04-10 19:35:49 +0200

    Make OSM import script configurable

--- import_osm.sh
+++ import_osm.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-osmosis --read-pbf data/koeln-regbez-latest.osm.pbf \
+DATA_PATH=data/koeln-regbez-latest.osm.pbf
+DB_HOST=127.0.0.1
+DB_USER=postgres
+DB_PASSWORD=postgres
+
+osmosis --read-pbf $DATA_PATH \
     --tag-filter reject-relations \
     --tag-filter accept-nodes shop=convenience,general,supermarket \
     --tag-filter accept-ways shop=convenience,general,supermarket \
-    --write-pgsql database=shopalone host=127.0.0.1 user=postgres password=postgres
+    --write-pgsql database=shopalone host=$DB_HOST user=$DB_USER password=$DB_PASSWORD
