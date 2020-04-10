3d885f641bcf7983eadb2d5f949e25abe31faaa2
Julian Theis <julian.theis@posteo.de>
2020-04-10 22:23:39 +0200

    Actually plot load

--- src/web/plotter.py
+++ src/web/plotter.py
@@ -2,17 +2,31 @@ import io
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg
-from dateutil import parser
+from datetime import datetime
 
 
 def plot_visits(visits: list) -> bytes:
-    t = np.arange(0, 24*60)
-    f = np.sin(1/60 * t)
+    t = np.arange(0, 24, 1/60)
+    load = load_function(t, visits)
 
     fig, ax = plt.subplots()
-    ax.plot(t, f)
+    ax.plot(t, load)
 
     image = io.BytesIO()
     FigureCanvasAgg(fig).print_png(image)
 
     return image
+
+
+def load_function(t: np.ndarray, visits: list) -> np.ndarray:
+    today = datetime.today().date()
+    load = np.zeros(t.shape)
+
+    for visit in visits:
+        visit_time = visit.tstamp
+
+        if visit_time.date() == today:
+            visit_minutes = 60*visit_time.hour + visit_time.minute
+            load[visit_minutes:visit_minutes+30] += np.ones(30)
+
+    return load
