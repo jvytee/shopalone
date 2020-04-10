9361caea423878f2ad7da1ff9c71fdf8a91356d1
Julian Theis <julian.theis@posteo.de>
2020-04-10 23:28:42 +0200

    Fix launch.sh output

--- launch.sh
+++ launch.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+# Example setup for application deployment.
+# While this script uses Podman to mangage containers, the command can be
+# sustituded with Docker if needed.
+
 echo Launching PostGIS container
 sudo podman run --rm --name shopalone-postgis -d -e POSTGRES_PASSWORD=postgres -p 127.0.0.1:5432:5432 -v $(pwd)/data/postgres:/var/lib/postgresql/data postgis/postgis
 
-echo Starting Gunicorn application server
+echo -e "\nStarting Gunicorn application server"
 cd src
 gunicorn -w 2 shopalone:app
 
-echo Stopping PostGIS container
+echo -e "\nStopping PostGIS container"
 sudo podman stop shopalone-postgis
