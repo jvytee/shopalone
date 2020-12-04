2d846af95a4837c769c7fe51aee1afbf5d4d9cf0
Julian Theis <julian.theis@posteo.de>
2020-12-04 16:39:26 +0100

    Fix terminology

--- import_osm.sh
+++ import_osm.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
-#DATA_PATH=../data/koeln-regbez-latest.osm.pbf
-#DB_HOST=localhost
-
 if [ -z $3 ]; then
-    echo "Usage: $0 <PBF path> <PostgreSQL host> <PostgreSQL schema>"
+    echo "Usage: $0 <PBF path> <PostgreSQL host> <PostgreSQL database>"
     exit 0
 fi
 
 DATA_PATH=$1
 DB_HOST=$2
-DB_SCHEMA=$3
+DB_DATABASE=$3
 DB_USER=postgres
 DB_PASSWORD=postgres
 
@@ -18,4 +15,4 @@ osmosis --read-pbf $DATA_PATH \
     --tag-filter reject-relations \
     --tag-filter accept-nodes shop=convenience,general,supermarket \
     --tag-filter accept-ways shop=convenience,general,supermarket \
-    --write-pgsql database=shopalone host=$DB_HOST user=$DB_USER password=$DB_PASSWORD
+    --write-pgsql database=$DB_DATABASE host=$DB_HOST user=$DB_USER password=$DB_PASSWORD
